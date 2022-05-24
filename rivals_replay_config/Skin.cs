b19bd75b97aa50ba18642d8a7f6b0579b39165bb
benwang2 <eb.benwang@gmail.com>
2022-05-23 20:55:22 -0400

    Updated Skins.cs to include 41-44 as Online Customs and cleaned up constructor.

--- rivals_replay_config/Skin.cs
+++ rivals_replay_config/Skin.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace rivals_replay_config { 
 
     class Skin
     {
+        public static Regex onlineCustomPatt = new Regex(@"[2-4][1-4]");
         public static Dictionary<string, string> defaultSkins = new Dictionary<string, string>();
-        private Skins skins;
-        private Dictionary<string, string> characterSpecific;
+        public static Skins skins = new Skins();
         private string name;
         private string id;
 
@@ -23,22 +24,22 @@ namespace rivals_replay_config {
             defaultSkins.Add("04", "Default 5");
             defaultSkins.Add("05", "Default 6");
             defaultSkins.Add("06", "Seasonal");
-            defaultSkins.Add("21", "Custom 1");
-            defaultSkins.Add("22", "Custom 2");
+            defaultSkins.Add("08", "Local Custom 1");
+            defaultSkins.Add("09", "Local Custom 2");
+            defaultSkins.Add("41", "Online Custom P1");
+            defaultSkins.Add("42", "Online Custom P2");
+            defaultSkins.Add("43", "Online Custom P3");
+            defaultSkins.Add("44", "Online Custom P4");
         }
 
         //"Default 1", "Default 2", "Default 3", "Default 4", "Default 5", "Default 6", "Seasonal", "Custom 1", "Custom 2"
 
-        public Skin(Character character, string skinId)
+        public Skin(Character character, string id)
         {
-            skins = new Skins();
-            characterSpecific = skins.getSpecificSkins(character.getName());
-
-            if (!characterSpecific.TryGetValue(skinId, out name))
-            {
-                defaultSkins.TryGetValue(skinId, out name);
-            }
-            id = skinId;
+            this.id = id;
+            Dictionary<string, string> characterSpecific = skins.getSpecificSkins(character.getName());
+            if (!characterSpecific.TryGetValue(id, out name))
+                defaultSkins.TryGetValue(id, out name);
         }
 
         public string getName() { return name; }
